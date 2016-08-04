42ad101c75241659eae40efd5b7f3aec34b02595
Adam Nemeth <adamnemeth.contact@gmail.com>
2016-08-04 17:16:13 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/whoami', function (req, res) {
+    var ip = req.headers["x-forwarded-for"]
+    var language = req.headers["accept-language"]
+    language = language.split(",")
+    var software = req.headers["user-agent"]
+    var start = software.indexOf("(")
+    var end = software.indexOf(")")
+    var fin = software.substring(start+1,end)
+    var back = {
+        "ipaddress": ip,
+        "language": language[0],
+        "software": fin
+    }
+  res.send(back);
 });
 
 app.listen(8080, function () {
